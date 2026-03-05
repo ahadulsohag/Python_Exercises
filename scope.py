288857d99610be3d4f21e65279126541487e8146
Sohag <ahadulsohag@gmail.com>
2026-03-06 02:27:53 +0600

    adding

--- scope.py
+++ scope.py
@@ -1,18 +1,30 @@
-x = 5
+# x = 5
 
-def set_x(num):
-    # Local var x not the same as global variable x
-    x = num  # Returns 43
-    return x        # Returns 43
+# def set_x(num):
+#     # Local var x not the same as global variable x
+#     x = num  # Returns 43
+#     return x        # Returns 43
 
-def set_global_x(num):
-    global x
-    x        # Returns 5
-    x = num  # global var x is now set to 6
-    return x        # Returns 6
+# def set_global_x(num):
+#     global x
+#     x        # Returns 5
+#     x = num  # global var x is now set to 6
+#     return x        # Returns 6
 
-print(set_x(43))
-print(set_global_x(6))
-print("Hello, World", end="!y",)
-print("hi")
-print("hi")
+# print(set_x(43))
+# print(set_global_x(6))
+# print("Hello, World", end="!y",)
+# print("hi")
+# print("hi")
+# x = 3   
+# x       # Prints "3"
+# y = type(x)
+
+# print(y)
+
+x = 1_0
+print(x)
+print(type(x))
+print(True+True + True) # Returns 2
+print(True * 8  )  # Returns 8
+print(False - 5)   # Returns -5
