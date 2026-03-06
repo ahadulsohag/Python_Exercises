e23af6533d5c0220b3c3e481d7c1b36593139532
Sohag <ahadulsohag@gmail.com>
2026-03-06 10:16:11 +0600

    practicing

---
+++ ex.py
@@ -0,0 +1,9 @@
+print('%10s'%('test',))
+print('{:>10}'.format('test'))
+test2 = "schoooooool"
+test1 = "umbrellaaaa"
+test0 = "elephantttt"
+
+print(f'{test2:3.93}, {test1:5.2}, {test0:2.1}') # Returns "tes       , te   , t "
+
+print('{:6d}'.format(42))
