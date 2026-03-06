c1708cd657c3b3daa4007d810e7b0979008e27a6
Sohag <ahadulsohag@gmail.com>
2026-03-06 12:32:47 +0600

    ex

--- ex.py
+++ ex.py
@@ -7,3 +7,11 @@ test0 = "elephantttt"
 print(f'{test2:3.93}, {test1:5.2}, {test0:2.1}') # Returns "tes       , te   , t "
 
 print('{:6d}'.format(42))
+print('{:+f}'.format(55))
+print("int: {0:d};  hex: {0:x};  oct: {0:o};  bin: {0:b}".format(42) )# Returns "int: 42;  hex: 2a;  oct: 52;  bin: 101010'"
+bin_43 = "{0:b}".format(43)
+bid = "{0:b}".format(43) 
+
+print(bid)
+print(bin_43)
+print('{:,}'.format(1234567890))
